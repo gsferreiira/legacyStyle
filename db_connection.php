04507728db7d27a7df2e0ca82c7cd543c66cbd03
unknown <gsferreiira@gmail.com>
2025-12-08 18:32:52 -0300

    consulta de banco de dados correta

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,10 @@
 <?php
-$host = "localhost";  // Padrão do XAMPP
-$dbname = "legacystyle";  // Nome do seu banco
-$username = "root";  // Usuário padrão do XAMPP
-$password = "";  // Senha vazia no XAMPP
+$host = "localhost"; // Na Hostinger, deixe 'localhost' mesmo
+
+// --- PREENCHA COM OS DADOS DA HOSTINGER ---
+$dbname = "u292075858_legacy"; // Nome do Banco (veja onde achar abaixo)
+$username = "u292075858_admin";     // Usuário do Banco
+$password = "Adminlegacy2000@";     // A senha que você criou para o banco
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
